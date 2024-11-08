f9da281d29ce4b74b54ec7b86d53b5e3c2d8d72c
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-07 23:50:35 -0600

    Restructuring: BD class

--- node/src/model/db.ts
+++ node/src/model/db.ts
@@ -1,37 +1,84 @@
 import { existsSync } from "fs"
 import sqlite from "sqlite3"
-
-class DBError extends Error {
-    constructor(message: string) {
-        super(message)
-        this.name = "DBError"
-    }
-}
+import { DBError } from "../api/util/error"
 
 export default class DB {
-    db!: sqlite.Database
+    db: sqlite.Database | null = null
 
     async open(path: string) {
-        if (!path) throw new DBError("Se requiere la ruta de la base de datos")
+        this.close()
+        if (!path)
+            throw new DBError({
+                message: DBError.ERROPEN,
+                error: {
+                    msg: "No se proporcionó ninguna ruta",
+                    location: "params",
+                    path: "path",
+                    value: path,
+                },
+            })
         if (!path.endsWith(".db"))
-            throw new DBError(`${path} no es un archivo de base de datos`)
-        if (!existsSync(path)) throw new DBError(`El archivo ${path} no existe`)
-        this.db = await this.getDataBase(path)
-        return this
+            throw new DBError({
+                message: DBError.ERROPEN,
+                error: {
+                    msg: `${path} no es un archivo de base de datos válido`,
+                    location: "params",
+                    path: "path",
+                    value: path,
+                },
+            })
+        if (!existsSync(path))
+            throw new DBError({
+                message: DBError.ERROPEN,
+                error: {
+                    msg: `El archivo ${path} no existe`,
+                    location: "params",
+                    path: "path",
+                    value: path,
+                },
+            })
+        try {
+            this.db = await this.getDataBase(path)
+            return this
+        } catch (e) {
+            const err = e as Error
+            throw new DBError({
+                message: DBError.ERROPEN,
+                error: {
+                    msg: err.message,
+                    location: "open",
+                    path: "method",
+                },
+            })
+        }
     }
 
     private getDataBase(path: string) {
         return new Promise<sqlite.Database>((resolve, reject) => {
             const db = new sqlite.Database(path, (err) => {
                 if (err) reject(err)
-                resolve(db)
+                else resolve(db)
             })
         })
     }
 
+    private checkDB() {
+        if (!this.db)
+            throw new DBError({
+                message: DBError.ERRQUERY,
+                error: {
+                    msg: "No hay base de datos abierta",
+                    location: "query",
+                    path: "method",
+                },
+            })
+        return this.db
+    }
+
     async fetch<T>(query: string, params: any[] = []) {
+        const db = this.checkDB()
         return new Promise<T[]>((resolve, reject) => {
-            this.db.all<T>(query, params, async (err, rows) => {
+            db.all<T>(query, params, async (err, rows) => {
                 if (err) reject(err)
                 /*  const casted = rows.map((row) => {
                     for (const key in row as Object) {
@@ -42,18 +89,17 @@ export default class DB {
                         }
                     }
                     return row
-                }) */
-                resolve(rows)
+                }) */ else resolve(rows)
             })
         })
     }
 
     async insert(query: string, params: any[] = []) {
-        const db = this.db
+        const db = this.checkDB()
         return new Promise((resolve, reject) => {
-            this.db.serialize(() => {
-                this.db.run("BEGIN TRANSACTION")
-                this.db.run(query, params, function (err) {
+            db.serialize(() => {
+                db.run("BEGIN TRANSACTION")
+                db.run(query, params, function (err) {
                     if (err) {
                         db.run("ROLLBACK")
                         reject(err)
@@ -67,19 +113,23 @@ export default class DB {
     }
 
     async lastID(table: string) {
+        const db = this.checkDB()
         return new Promise<number>((resolve, reject) => {
             const query = `
             SELECT COALESCE(MIN(id) + 1, 1) AS id
             FROM ${table} t1
             WHERE NOT EXISTS (SELECT 1 FROM ${table} t2 WHERE t2.id = t1.id + 1);`
-            this.db.get<TableID>(query, (err, row) => {
+            db.get<TableID>(query, (err, row) => {
                 if (err) reject(err)
-                resolve(row.id)
+                else resolve(row.id)
             })
         })
     }
 
     close() {
-        if (this.db) this.db.close()
+        if (this.db) {
+            this.db.close()
+            this.db = null
+        }
     }
 }
