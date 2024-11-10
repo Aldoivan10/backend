30e5ae62b53406433c96970fdb33b1f88d7ac8da
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-10 17:53:55 -0600

    Refactor: Error middleware to send error

--- node/src/api/middleware/errorMW.ts
+++ node/src/api/middleware/errorMW.ts
@@ -3,12 +3,16 @@ import { APIError } from "../util/error"
 import * as Logger from "../util/logger"
 
 export const errorMW = (
-    err: APIError | Error,
+    err: APIError,
     _: Request,
     res: Response,
     next: NextFunction
 ) => {
     Logger.error(err)
     res.locals.error = err
+    res.status(err.status).json({
+        message: err.message,
+        errors: err.errors || [],
+    })
     next()
 }
