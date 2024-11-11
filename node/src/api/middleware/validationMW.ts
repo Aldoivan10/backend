cdf964f3c44a1ced8f32950b61931b312025fc8b
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-10 23:26:08 -0600

    Refactor: Generalize validation

--- node/src/api/middleware/validationMW.ts
+++ node/src/api/middleware/validationMW.ts
@@ -1,15 +1,15 @@
 import { NextFunction, Request, Response } from "express"
-import { checkSchema, validationResult } from "express-validator"
+import { checkSchema, Schema, validationResult } from "express-validator"
 import { ValError } from "../util/error"
 import { getValError } from "../util/logger"
 
-export const validationMW = ({ schema, item }: ValMWArgs): any[] => {
+export const validationMW = (schema: Schema): any[] => {
     return [
         checkSchema(schema),
         (req: Request, _: Response, next: NextFunction) => {
             const errors = validationResult(req)
             if (!errors.isEmpty()) {
-                const msg = item ? ValError.ERRCREATE(item) : ValError.ERRVAL
+                const msg = ValError.ERRVAL
                 next(getValError(msg, errors.array(), 422))
             } else {
                 next()
