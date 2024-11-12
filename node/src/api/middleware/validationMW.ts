b47257bd84c07e0cf64d9fe138078e87ced91b43
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-12 11:09:41 -0600

    Refactor: Validation middleware to pass schema or custom checkSchema

--- node/src/api/middleware/validationMW.ts
+++ node/src/api/middleware/validationMW.ts
@@ -3,17 +3,15 @@ import { checkSchema, Schema, validationResult } from "express-validator"
 import { ValError } from "../util/error"
 import { getValError } from "../util/logger"
 
-export const validationMW = (schema: Schema): any[] => {
+export const validationMW = (arg: Schema | Function): any[] => {
     return [
-        checkSchema(schema),
+        arg instanceof Function ? arg : checkSchema(arg),
         (req: Request, _: Response, next: NextFunction) => {
             const errors = validationResult(req)
             if (!errors.isEmpty()) {
                 const msg = ValError.ERRVAL
                 next(getValError(msg, errors.array(), 422))
-            } else {
-                next()
-            }
+            } else next()
         },
     ]
 }
