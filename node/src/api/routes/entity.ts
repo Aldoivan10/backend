132237dfbece7af44d9549c3f0997d30593a34da
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-13 19:09:59 -0600

    Refactor: Functions to CRUD

---
+++ node/src/api/routes/entity.ts
@@ -0,0 +1,135 @@
+import { NextFunction, Request, Response, Router } from "express"
+import { validationMW } from "../middleware/validationMW"
+import { getBase } from "../util/util"
+import { entityVal } from "../validations/entityVal"
+import * as GeneralVal from "../validations/generalVal"
+
+const router = Router()
+const table = "Entidad"
+
+const getParams = (req: Request) => {
+    const {
+        rfc,
+        name,
+        phone,
+        email,
+        address,
+        domicile,
+        postal_code,
+        id_entity_type,
+    }: EntityBody = req.body
+
+    return [
+        id_entity_type,
+        rfc,
+        name,
+        address,
+        domicile,
+        postal_code,
+        phone,
+        email,
+    ]
+}
+
+// Obtener todos
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { db, limit, offset, filter } = getBase(req)
+        const entitys = await db.all<Entity>(
+            table,
+            ["id", "nombre as Name"],
+            [`%${filter}%`, limit, offset]
+        )
+        res.json({ data: entitys })
+    } catch (err) {
+        next(err)
+    }
+})
+
+// Obtener por ID
+router.get(
+    "/:id(\\d+)",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { db, id } = getBase(req)
+            const entity = await db.getByID<Entity>(
+                table,
+                ["id", "nombre AS name"],
+                id
+            )
+            res.json({ data: entity ?? null })
+        } catch (err) {
+            next(err)
+        }
+    }
+)
+
+// Insertar
+router.post(
+    "/",
+    validationMW(entityVal),
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { db } = getBase(req)
+            const params = getParams(req)
+            const entity = await db.insert<Entity>(table, params)
+            res.status(201).json({
+                message: "Entidad creada",
+                data: entity,
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+)
+
+// Eliminar
+router.delete(
+    "/",
+    validationMW(GeneralVal.ids),
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { db, ids } = getBase(req)
+            const entitys = await db.delete<Entity>(table, ids)
+            res.send({ message: "Entidades eliminadas", data: entitys })
+        } catch (err) {
+            next(err)
+        }
+    }
+)
+
+// Actualizar nombre
+router.patch(
+    "/:id(\\d+)",
+    validationMW(entityVal),
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { db, id } = getBase(req)
+            const params = getParams(req)
+            const entity = await db.update<Entity>(
+                table,
+                [
+                    "id_tipo_entidad",
+                    "rfc",
+                    "nombre",
+                    "direccion",
+                    "domicilio",
+                    "codigo_postal",
+                    "telefono",
+                    "correo",
+                ],
+                id,
+                params
+            )
+
+            res.send({
+                message: "Entidad actualizada",
+                data: entity,
+            })
+        } catch (err) {
+            next(err)
+        }
+    }
+)
+
+export default router
