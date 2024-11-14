d791f76575debb264bb11804d9244423927d4121
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-14 11:48:04 -0600

    Added: Get base parameters function

--- node/src/api/util/util.ts
+++ node/src/api/util/util.ts
@@ -1,4 +1,13 @@
-export const plural = (word: string) => {
-    if (word.endsWith("d")) return word + "es"
-    return word + "s"
+import { Request } from "express"
+
+export const getPlaceholders = (arr: any[]) => {
+    return arr.map((_) => "?").join(", ")
+}
+
+export const getBase = (req: Request) => {
+    const { limit = 10, offset = 0, filter = "" }: APIFilter = req.query
+    const db = req.app.locals.db
+    const id: number = +req.params.id
+    const ids: number[] = req.body.ids ?? []
+    return { db, id, ids, limit, offset, filter }
 }
