7fccdc0e3701e706d7b93750f5a7344748e2bde8
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-15 16:03:16 -0600

    refactor: Now error functions is in, and removed unused attribute

--- node/src/api/util/error.ts
+++ node/src/api/util/error.ts
@@ -1,22 +1,15 @@
+import { ValidationError } from "express-validator"
+
 export class APIError extends Error {
     errors: APIErrorsArr = []
     name: string = "APIError"
-    request?: string
     status: number
 
-    constructor({
-        status = 500,
-        message,
-        error,
-        errors,
-        name,
-        request,
-    }: APIErrorArgs) {
+    constructor({ message, error, errors, name, status = 500 }: APIErrorArgs) {
         super(message)
         if (error) this.errors.push({ ...error, type: this.name })
         if (errors) this.errors = errors
         if (name) this.name = name
-        if (request) this.request = request
         this.status = status
     }
 }
@@ -37,4 +30,40 @@ export class DBError extends APIError {
     static ERRINSERT = "Error al insertar datos"
     static ERRUPDATE = "Error al actualizar datos"
     static ERRDELETE = "Error al eliminar datos"
+}
+
+export const getError = (err: Error, status = 500) => {
+    const { message, stack } = err
+    let path = stack || "unknown"
+    let location = "unknown"
+    let msg = message
+
+    if (message.startsWith("SQL")) {
+        const [name, ...data] = message.split(": ")
+        path = name
+        msg = data.join(": ")
+        location = "database"
+    }
+
+    return new APIError({
+        message: "A ocurrido un error inesperado",
+        status,
+        error: {
+            location,
+            path,
+            msg,
+        },
+    })
+}
+
+export const getValError = (
+    msg: string,
+    errors: ValidationError[],
+    status = 400
+) => {
+    return new ValError({
+        message: msg,
+        errors,
+        status,
+    })
 }
