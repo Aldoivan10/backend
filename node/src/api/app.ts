f0d39f922374d00d182a53acb1e22ff0accf9ef9
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-16 16:01:58 -0600

    Add: Login route

--- node/src/api/app.ts
+++ node/src/api/app.ts
@@ -1,8 +1,10 @@
+import cookieParser from "cookie-parser"
 import express, { json } from "express"
-import DB from "../model/db"
 import { HOST, PORT } from "./config"
 import { errorMW } from "./middleware/errorMW"
 import { logMW } from "./middleware/logMW"
+import DB from "./model/db"
+import auth from "./routes/auth"
 import catalogRoute from "./routes/catalog"
 import entityRoute from "./routes/entity"
 import productRoute from "./routes/product"
@@ -12,18 +14,22 @@ const app = express()
 
 // Deshabilitar el header X-Powered-By
 app.disable("x-powered-by")
+// Middleware para logs de peticiones
+app.use(logMW)
 // Middleware para parsear el body de la petición
 app.use(json())
+// Middleware para las cookies
+app.use(cookieParser())
 // Ignorar peticiones a favicon
 app.get("/favicon.ico", (_, res) => {
     res.status(204)
 })
-// Middleware para logs de peticiones
-app.use(logMW)
 // Pagina de bienvenida
 app.get("/", (_, res) => {
     res.status(200).json({ message: "API con express", dev: "Aldoivan" })
 })
+// Ruta para la autenticación de usuarios
+app.use(auth)
 // Rutas
 app.use("/product", productRoute)
 app.use("/entity", entityRoute)
