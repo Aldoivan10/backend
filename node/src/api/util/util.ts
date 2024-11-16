150c7f73c0dc6a6659c97be547c78525cfe1aff4
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-16 12:28:03 -0600

    Add: Validation of password function

--- node/src/api/util/util.ts
+++ node/src/api/util/util.ts
@@ -1,13 +1,21 @@
+import bcrypt from "bcrypt"
 import { Request } from "express"
 
+// Creamos un placeholder de ? para un arreglo
 export const getPlaceholders = (arr: any[]) => {
     return arr.map((_) => "?").join()
 }
 
+// Obtenemos los datos base de cada petición
 export const getBase = (req: Request) => {
     const { limit = 10, offset = 0, filter = "" }: APIFilter = req.query
     const db = req.app.locals.db
     const id: number = +req.params.id
     const ids: number[] = req.body.ids ?? []
     return { db, id, ids, limit, offset, filter }
+}
+
+// Validamos que la contraseña sea correcta
+const isPass = (pass: string, hashed: string) => {
+    return bcrypt.compareSync(pass, hashed)
 }
