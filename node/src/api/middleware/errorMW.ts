c816a2022bcd28ee47fbe242f38f1f044e7e6bf1
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-15 20:31:32 -0600

    Fix: Add last require parameter

--- node/src/api/middleware/errorMW.ts
+++ node/src/api/middleware/errorMW.ts
@@ -1,7 +1,12 @@
-import { Request, Response } from "express"
+import { NextFunction, Request, Response } from "express"
 import { APIError, getError } from "../util/error"
 
-export const errorMW = (err: APIError | Error, _: Request, res: Response) => {
+export const errorMW = (
+    err: APIError | Error,
+    _: Request,
+    res: Response,
+    __: NextFunction
+) => {
     const error = err instanceof APIError ? err : getError(err)
     res.locals.error = error
     res.status(error.status).json({
