c5d5989a01892fbf7708ed798abf0a81a485beb0
Aldoivan <aldoivan_palma@hotmail.com>
2024-11-16 15:57:29 -0600

    Refactor: imports and validation error generator

--- node/src/api/middleware/validationMW.ts
+++ node/src/api/middleware/validationMW.ts
@@ -1,16 +1,15 @@
 import { NextFunction, Request, Response } from "express"
 import { checkSchema, Schema, validationResult } from "express-validator"
-import { getValError, ValError } from "../util/error"
+import { ValError } from "../model/error"
 
 export const validationMW = (arg: Schema | Function): any[] => {
     return [
         arg instanceof Function ? arg : checkSchema(arg),
         (req: Request, _: Response, next: NextFunction) => {
             const errors = validationResult(req)
-            if (!errors.isEmpty()) {
-                const msg = ValError.ERRVAL
-                next(getValError(msg, errors.array(), 422))
-            } else next()
+            if (!errors.isEmpty())
+                next(new ValError({ errors: errors.array() }))
+            else next()
         },
     ]
 }
