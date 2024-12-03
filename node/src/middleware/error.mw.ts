b9f737a16c8d10c7fb15dd8112d5d01f75c0801d
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-03 15:45:45 -0600

    Refactor: API error message

---
+++ node/src/middleware/error.mw.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express"
+import { APIError } from "../model/error"
+import { getError } from "../util/error"
+
+export const errorMW = (
+    err: APIError | Error,
+    _: Request,
+    res: Response,
+    __: NextFunction
+) => {
+    const error = err instanceof APIError ? err : getError(err)
+    res.locals.error = error
+    res.status(error.status).json({
+        message: error.message,
+        errors: error.details || [],
+    })
+}
