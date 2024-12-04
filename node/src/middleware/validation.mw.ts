933383c5e10b2fcf73dc4f01928fae1b9508b09b
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-03 20:38:39 -0600

    Refactor: Validation error, send array of details

---
+++ node/src/middleware/validation.mw.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express"
+import { checkSchema, Schema, validationResult } from "express-validator"
+import { ValidError } from "../model/error"
+
+export const validationMW = (arg: Schema | Function): any[] => {
+    return [
+        arg instanceof Function ? arg : checkSchema(arg),
+        (req: Request, _: Response, next: NextFunction) => {
+            const errors = validationResult(req)
+            if (!errors.isEmpty())
+                throw ValidError.fromExpress(errors.array() as any)
+            next()
+        },
+    ]
+}
