dcca8a923712ae136a8b1d603ee7313b23e1c0c1
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-04 19:40:00 -0600

    Add: Catalog repository

---
+++ node/src/repositories/catalog.repo.ts
@@ -0,0 +1,51 @@
+import { Statement } from "better-sqlite3"
+import { toBD } from "../util/util"
+import Repository from "./repository"
+
+export default class CatalogRepository extends Repository<CatalogItem> {
+    protected mapper: Record<string, string> = {
+        id: "id",
+        name: "nombre",
+    }
+    private insertStm?: Statement<CatalogItem, CatalogItem>
+    private updateStm?: Statement<CatalogItem, CatalogItem>
+
+    constructor() {
+        super("Catalogo")
+    }
+
+    setTable(table: string) {
+        this.table = table
+        this.init({ columns: Object.values(this.mapper) })
+        this.insertStm = this.db.prepare<CatalogItem, CatalogItem>(
+            `INSERT INTO ${table} VALUES (@id, @name) RETURNING *`
+        )
+        this.updateStm = this.db.prepare<CatalogItem, CatalogItem>(
+            `UPDATE ${table} SET nombre=@name WHERE id=@id RETURNING *`
+        )
+        return this
+    }
+
+    insert(item: CatalogItem): CatalogItem {
+        if (this.insertStm) {
+            const id = this.nextID()!
+            const catalog = toBD<CatalogItem>(
+                { ...item, id },
+                Object.keys(this.mapper)
+            )
+            return this.mapFunc(this.insertStm.get(catalog))!
+        }
+        throw new Error("Method not implemented.")
+    }
+
+    update(id: number, item: CatalogItem): Maybe<CatalogItem> {
+        if (this.updateStm) {
+            const catalog = toBD<CatalogItem>(
+                { ...item, id },
+                Object.keys(this.mapper)
+            )
+            return this.mapFunc(this.updateStm.get(catalog))
+        }
+        throw new Error("Method not implemented.")
+    }
+}
