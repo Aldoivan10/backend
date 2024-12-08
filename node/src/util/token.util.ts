fe2bcd1873e1daffc0b1dc41fb48495cc5923a7e
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-07 19:37:36 -0600

    Create: Token utils functions

---
+++ node/src/util/token.util.ts
@@ -0,0 +1,28 @@
+import { Request } from "express"
+import { jwtVerify, SignJWT } from "jose"
+import { TK_ALG, TK_KEY } from "../config"
+
+const encoder = new TextEncoder()
+
+// Funcion para obtener el usuario (payload)
+export const getUser = async (token?: string) => {
+    if (!token) return undefined
+    const { payload } = await jwtVerify(token, encoder.encode(TK_KEY))
+    return payload as Maybe<UserToken>
+}
+
+// Funcion para obtener el token
+export const singToken = async (payload: any, time: string) => {
+    const token = await new SignJWT(payload)
+        .setExpirationTime(time)
+        .setProtectedHeader({ alg: TK_ALG })
+        .sign(encoder.encode(TK_KEY))
+    return token
+}
+
+// Obtenemos los tokens de acceso
+export const getTokens = (req: Request) => {
+    const { name }: LoginBody = req.body
+    const cookies = req.cookies
+    return [cookies[`${name}_at`], cookies[`${name}_rt`]]
+}
