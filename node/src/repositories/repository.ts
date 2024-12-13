3645cecf07ebb23142da2cb683172bff39404e4b
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-12 19:09:47 -0600

    Refactor: Now requiere insert and update declarations, that should be a Stament or Transaction (for log), now delete save deletes in log

--- node/src/repositories/repository.ts
+++ node/src/repositories/repository.ts
@@ -1,13 +1,22 @@
-import { Database, Statement } from "better-sqlite3"
+import { Database, Statement, Transaction } from "better-sqlite3"
 import db from "../model/db"
-import { getPlaceholders, mapTo, notFalsy, toJSON } from "../util/util"
+import { arrConj, getPlaceholders } from "../util/array.util"
+import { mapTo, notFalsy, toJSON } from "../util/util"
 
-export default abstract class Repository<I, O> {
+export default abstract class Repository<
+    I extends CatalogBody,
+    O extends CatalogItem
+> {
     protected db: Database = db
     protected table: string
     protected allStm!: Statement<Filters, Obj>
     protected getByIDStm!: Statement<ID, Maybe<Obj>>
+    protected deleteStm!: Transaction<(args: DeleteArgs) => Array<Maybe<Obj>>>
     protected mapper?: Record<string, string>
+    protected abstract insertStm: Statement<I, O> | Transaction<(input: I) => O>
+    protected abstract updateStm:
+        | Statement<I, O>
+        | Transaction<(input: I, id: number) => O>
     protected mapFunc = (item: Maybe<Obj>) =>
         mapTo<O>(toJSON<Obj>(item), this.mapper)
 
@@ -20,8 +29,35 @@ export default abstract class Repository<I, O> {
         order = "nombre",
         filter = "nombre",
     }: RepoArgs) => {
+        const deleteLogStm = this.db.prepare<CatalogItem, unknown>(
+            "INSERT INTO Log VALUES (@id, @name)"
+        )
+        const deleteDescStm = this.db.prepare<ID & { desc: string }, unknown>(
+            "INSERT INTO Log_Cambio VALUES (@id, @desc)"
+        )
+
         this.allStm = this.db.prepare(this.allQuery(columns, order, filter))
         this.getByIDStm = this.db.prepare(this.getByIDQuery(columns))
+        this.deleteStm = this.db.transaction(
+            ({ ids, username, table = this.table }) => {
+                const placeholders = getPlaceholders(ids)
+                const stm = this.db.prepare<number[], Obj>(
+                    `DELETE FROM ${this.table} WHERE id IN (${placeholders}) RETURNING *`
+                )
+                const items = stm
+                    .all(...ids)
+                    .map(this.mapFunc)
+                    .filter(notFalsy)
+                const logID = this.nextID("Log")!
+                const names = items.map((item) => item.name)
+                const desc = `<strong>${username} <span class="danger">eliminó</span></strong> (${table}): ${arrConj(
+                    names
+                )}`
+                deleteLogStm.run({ id: logID, name: username })
+                deleteDescStm.run({ id: logID, desc })
+                return items
+            }
+        )
     }
 
     public all(filter: Filters) {
@@ -32,38 +68,31 @@ export default abstract class Repository<I, O> {
         return this.mapFunc(this.getByIDStm.get({ id }))
     }
 
-    public delete(ids: number[]) {
-        const placeholders = getPlaceholders(ids)
-        const stm = this.db.prepare<number[], Obj>(
-            `DELETE FROM ${this.table} WHERE id IN (${placeholders}) RETURNING *`
-        )
-        return stm
-            .all(...ids)
-            .map(this.mapFunc)
-            .filter(notFalsy)
+    public delete(args: DeleteArgs) {
+        return this.deleteStm(args)
     }
 
     abstract insert(item: I): O
 
     abstract update(id: number, item: I): Maybe<O>
 
-    protected allQuery(columns: string[], orderBy?: string, filterBy?: string) {
+    private allQuery(columns: string[], orderBy?: string, filterBy?: string) {
         const cols = columns.join()
         const filter = filterBy ? ` WHERE ${filterBy} LIKE @filter ` : ""
         const order = orderBy ? ` ORDER BY ${orderBy} ` : ""
         return `SELECT ${cols} FROM ${this.table}${filter}${order} LIMIT @limit OFFSET @offset`
     }
 
-    protected getByIDQuery(columns: string[]) {
+    private getByIDQuery(columns: string[]) {
         const cols = columns.join()
         return `SELECT ${cols} FROM ${this.table} WHERE id = @id`
     }
 
-    protected nextID() {
+    protected nextID(table?: string) {
         const query = `
         WITH miss_id AS
         (
-            SELECT id FROM ${this.table}
+            SELECT id FROM ${table ?? this.table}
             UNION ALL 
             SELECT 0
         )
@@ -71,8 +100,8 @@ export default abstract class Repository<I, O> {
         FROM miss_id
         WHERE NOT EXISTS
         (
-            SELECT * FROM ${this.table} 
-            WHERE ${this.table}.id = miss_id.id + 1
+            SELECT * FROM ${table ?? this.table} 
+            WHERE ${table ?? this.table}.id = miss_id.id + 1
         )`
         return this.db.prepare<[], ID>(query).get()?.id
     }
