886eac6202a391551b6db60a6e03480dc971feb8
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-17 23:25:56 -0600

    Add: DTO types

---
+++ node/src/dtos/dto.d.ts
@@ -0,0 +1,9 @@
+declare namespace DTO {
+    type TransformFilters = {
+        value: Record<string, string | number | boolean>
+    }
+
+    type TransformOrders = {
+        value: string[]
+    }
+}
