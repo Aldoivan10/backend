3ee4b83cff256e5c619965d2992826057671d94e
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-17 23:24:27 -0600

    Refactor: Simplify functions

--- node/src/dtos/filter.dto.ts
+++ node/src/dtos/filter.dto.ts
@@ -3,19 +3,19 @@ import { IsArray, IsNumber, IsObject, IsOptional, Min } from "class-validator"
 
 const transformFilterFn = ({ value }: DTO.TransformFilters) => {
     const regex = /[a-z0-9]+(_(gt|gte|lt|lte|eq|like)$)?/i
-    return Object.entries(value).reduce((obj, [key, val]) => {
-        if (regex.test(key))
-            obj[key] = typeof val === "string" ? val : Number(val)
-        return obj
-    }, {} as Record<string, string | number>)
+    return Object.fromEntries(
+        Object.entries(value)
+            .filter(([key]) => regex.test(key))
+            .map(([key, val]) => [
+                key,
+                typeof val === "string" ? val : Number(val),
+            ])
+    )
 }
 
 const transformOrderFn = ({ value }: DTO.TransformOrders) => {
     const regex = /[a-z0-9]+(_(asc|desc)$)?/i
-    return value.reduce((arr, item) => {
-        if (regex.test(item)) arr.push(item)
-        return arr
-    }, [] as string[])
+    return value.filter((item) => regex.test(item))
 }
 
 export class FilterDto {
