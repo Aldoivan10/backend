d618bb6f656ab8ee18ce834c9f0e5f91dbcc1fa6
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-24 17:43:09 -0600

    Refactor: User mapping

--- node/src/services/auth.svc.ts
+++ node/src/services/auth.svc.ts
@@ -1,10 +1,14 @@
 import bcrypt from "bcrypt"
-import { plainToClass } from "class-transformer"
+import { ClassTransformOptions, plainToClass } from "class-transformer"
 import { InitUser, UserDTO } from "../dtos/user.dto"
 import { AuthRepo } from "../repositories/auth.repo"
 
 export class AuthService {
     private repo = new AuthRepo()
+    private classOptions: ClassTransformOptions = {
+        groups: ["admin"],
+        excludeExtraneousValues: true,
+    }
 
     public availableUsers() {
         const users = this.repo
@@ -14,12 +18,20 @@ export class AuthService {
     }
 
     public authenticate(username: string) {
-        const user = plainToClass(UserDTO, this.repo.auth(username))
+        const user = plainToClass(
+            UserDTO,
+            this.repo.auth(username),
+            this.classOptions
+        )
         return user.payload
     }
 
     public login(payload: TokenPayload, password?: Maybe<string>) {
-        const user = plainToClass(UserDTO, this.repo.auth(payload.name))
+        const user = plainToClass(
+            UserDTO,
+            this.repo.auth(payload.name),
+            this.classOptions
+        )
         if (!this.isPassword(password, user.password)) return false
         payload.logged = true
         return true
