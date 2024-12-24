7197f207d871e86b486717ad91b8c6aead6602bd
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-24 17:52:04 -0600

    Refactor: Check if user is admin

--- node/src/routes/auth.route.ts
+++ node/src/routes/auth.route.ts
@@ -48,7 +48,7 @@ router.post(
             const user = res.locals.user
 
             if (!user) throw AuthError.token()
-            if (user.role.name !== "Administrador") throw AuthError.rol()
+            if (!user.admin) throw AuthError.rol()
             if (!svc.login(user, password)) throw AuthError.auth()
 
             const accessToken = await singToken(user, AT_TIME)
