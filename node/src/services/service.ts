2d1493e81b1d290ce8501912fbc4aa2c2d21f580
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-24 17:28:44 -0600

    Refactor: Add optional group to mapper

--- node/src/services/service.ts
+++ node/src/services/service.ts
@@ -1,4 +1,4 @@
-import { ClassConstructor, plainToClass } from "class-transformer"
+import { ClassConstructor, plainToInstance } from "class-transformer"
 import { FilterDomain } from "../domains/filter.domain"
 import { Repository } from "../repositories/repository"
 import { notFalsy } from "../utils/obj.util"
@@ -8,39 +8,58 @@ export abstract class Service<I extends Record<string, any>, O> {
 
     constructor(protected readonly dto: ClassConstructor<O>) {}
 
-    public all(filter: FilterDomain) {
+    public all(filter: FilterDomain, groups?: string[]) {
         return this.repo
             .all(filter.getData(), filter.getFilter())
-            .map(this.mapper.bind(this))
+            .map((item) => this.mapper(item, groups))
             .filter(notFalsy)
     }
 
-    public getByID(id: number) {
-        return this.mapper(this.repo.getByID(id))
+    public getByID(id: number, groups?: string[]) {
+        return this.mapper(this.repo.getByID(id), groups)
     }
 
-    protected insert(item: I, user: string): O {
-        return this.mapper(this.repo.insert(item, user))!
+    protected insert(item: I, user: string, groups?: string[]): O {
+        return this.mapper(this.repo.insert(item, user), groups)!
     }
 
-    protected update(id: number, item: I, user: string): Maybe<O> {
-        return this.mapper(this.repo.update(id, item, user))
+    protected update(
+        id: number,
+        item: I,
+        user: string,
+        groups?: string[]
+    ): Maybe<O> {
+        return this.mapper(this.repo.update(id, item, user), groups)
     }
 
-    protected delete(ids: number[], user: string, desc: string) {
+    protected delete(
+        ids: number[],
+        user: string,
+        desc: string,
+        groups?: string[]
+    ) {
         return this.repo
             .delete(ids, user, desc)
-            .map(this.mapper.bind(this))
+            .map((item) => this.mapper(item, groups))
             .filter(notFalsy)
     }
 
-    abstract add(body: I, username: string): O
+    abstract add(body: I, username: string, groups?: string[]): O
 
-    abstract edit(id: number, body: I, username: string): Maybe<O>
+    abstract edit(
+        id: number,
+        body: I,
+        username: string,
+        groups?: string[]
+    ): Maybe<O>
 
-    abstract remove(ids: number[], username: string): O[]
+    abstract remove(ids: number[], username: string, groups?: string[]): O[]
 
-    protected mapper(item: Maybe<Obj>) {
-        return item ? plainToClass(this.dto, item) : null
+    protected mapper(item: Maybe<Obj>, groups?: string[]) {
+        if (!item) return null
+        return plainToInstance(this.dto, item, {
+            groups,
+            excludeExtraneousValues: true,
+        })
     }
 }
