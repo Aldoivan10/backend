ee8344f381290558e7955baad45d3027be3ce2d4
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-24 21:10:31 -0600

    Removed: Unused decorators

--- node/src/dtos/user.dto.ts
+++ node/src/dtos/user.dto.ts
@@ -1,5 +1,5 @@
 import { Exclude, Expose, Transform, Type } from "class-transformer"
-import { IsNotEmpty, IsString, Min } from "class-validator"
+import { IsString, Min } from "class-validator"
 
 const transformShortcuts = (shortcuts: DTO.Shortcut[], view: 0 | 1) => {
     return shortcuts
@@ -17,21 +17,16 @@ class Shortcut {
 
 export class InitUser {
     @Expose({ name: "nombre" })
-    @Type(() => String)
-    @IsNotEmpty()
-    @IsString()
     declare name: string
 
     @Exclude()
     declare atajos: DTO.Shortcut[]
 
     @Expose() // Incluimos views como un atributo constante en la salida
-    @Type(() => Shortcut)
     @Transform(({ obj }) => transformShortcuts(JSON.parse(obj.atajos), 1))
     declare views: Shortcut[]
 
     @Expose() // Incluimos shortcuts como un atributo constante en la salida
-    @Type(() => Shortcut)
     @Transform(({ obj }) => transformShortcuts(JSON.parse(obj.atajos), 0))
     declare shortcuts: Shortcut[]
 }
@@ -43,19 +38,14 @@ export class UserDTO {
     declare id: number
 
     @Expose({ name: "nombre" })
-    @Type(() => String)
-    @IsNotEmpty()
     @IsString()
     declare name: string
 
     @Expose()
-    @Type(() => Object)
-    @IsNotEmpty()
     @Transform(({ obj }) => ({ id: obj.id_rol, name: obj.rol }))
     declare role: Record<string, any>
 
     @Expose({ groups: ["admin"] })
-    @Type(() => Boolean)
     @Transform((_) => false)
     declare logged: boolean
 
