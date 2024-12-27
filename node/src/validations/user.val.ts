11bb1499669eaf14eca605fef3ec8475db51bdff
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-26 20:39:28 -0600

    Add: Admin user require password

--- node/src/validations/user.val.ts
+++ node/src/validations/user.val.ts
@@ -7,6 +7,7 @@ import {
     nullish,
     number,
     object,
+    partialCheck,
     pipe,
     required,
     string,
@@ -14,31 +15,41 @@ import {
 } from "valibot"
 import { NameSchema } from "./general.val"
 
-export const UserSchema = intersect([
-    NameSchema(64),
-    required(
-        object({
-            password: nullish(
-                pipe(
-                    string("La contraseña debe ser una cadena"),
-                    trim(),
-                    nonEmpty("La contraseña no debe estar vacía"),
-                    minLength(
-                        3,
-                        "La contraseña debe de ser de al menos 3 carácteres"
+export const UserSchema = pipe(
+    intersect([
+        NameSchema(64),
+        required(
+            object({
+                password: nullish(
+                    pipe(
+                        string("La contraseña debe ser una cadena"),
+                        trim(),
+                        nonEmpty("La contraseña no debe estar vacía"),
+                        minLength(
+                            3,
+                            "La contraseña debe de ser de al menos 3 carácteres"
+                        ),
+                        maxLength(
+                            10,
+                            "La contraseña no debe exceder 10 carácteres"
+                        )
                     ),
-                    maxLength(10, "La contraseña no debe exceder 10 carácteres")
+                    null
                 ),
-                null
-            ),
-            id_user_type: pipe(
-                number("El ID de tipo de usuario debe ser un número"),
-                minValue(
-                    1,
-                    "El ID de tipo de usuario debe ser un número positivo"
-                )
-            ),
-        }),
-        "El tipo de usuario es obligatorio"
-    ),
-])
+                id_user_type: pipe(
+                    number("El ID de tipo de usuario debe ser un número"),
+                    minValue(
+                        1,
+                        "El ID de tipo de usuario debe ser un número positivo"
+                    )
+                ),
+            }),
+            "El tipo de usuario es obligatorio"
+        ),
+    ]),
+    partialCheck(
+        [["id_user_type"], ["password"]],
+        (input) => input.id_user_type === 1 && Boolean(input.password),
+        "Este tipo de usuario requiere contraseña"
+    )
+)
