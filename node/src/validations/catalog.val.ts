c2c1f7304782110da1b18bd4220aa6ee770316a8
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-26 20:02:01 -0600

    Refactor: Switch name validation from attribute to schema

--- node/src/validations/catalog.val.ts
+++ node/src/validations/catalog.val.ts
@@ -1,20 +1,9 @@
-import { object } from "valibot"
-import { nameAttr } from "../utils/val.uti"
+import { NameSchema } from "./general.val"
 
 export default {
-    code: object({
-        name: nameAttr(128),
-    }),
-    entity_type: object({
-        name: nameAttr(128),
-    }),
-    unit: object({
-        name: nameAttr(32),
-    }),
-    department: object({
-        name: nameAttr(128),
-    }),
-    user_type: object({
-        name: nameAttr(32),
-    }),
+    code: NameSchema(128),
+    entity_type: NameSchema(128),
+    unit: NameSchema(32),
+    department: NameSchema(128),
+    user_type: NameSchema(32),
 }
