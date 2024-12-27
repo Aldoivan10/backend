3104dd22ddf47300fcb90e421fd60b40e227b260
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-26 20:14:27 -0600

    Refactor: Remove logic from validations

--- node/src/validations/user.val.ts
+++ node/src/validations/user.val.ts
@@ -10,45 +10,35 @@ import {
     pipe,
     required,
     string,
-    transform,
     trim,
 } from "valibot"
 import { NameSchema } from "./general.val"
 
-export const UserSchema = pipe(
-    intersect([
-        NameSchema(64),
-        required(
-            object({
-                password: nullish(
-                    pipe(
-                        string("La contraseña debe ser una cadena"),
-                        trim(),
-                        nonEmpty("La contraseña no debe estar vacía"),
-                        minLength(
-                            3,
-                            "La contraseña debe de ser de al menos 3 carácteres"
-                        ),
-                        maxLength(
-                            10,
-                            "La contraseña no debe exceder 10 carácteres"
-                        )
+export const UserSchema = intersect([
+    NameSchema(64),
+    required(
+        object({
+            password: nullish(
+                pipe(
+                    string("La contraseña debe ser una cadena"),
+                    trim(),
+                    nonEmpty("La contraseña no debe estar vacía"),
+                    minLength(
+                        3,
+                        "La contraseña debe de ser de al menos 3 carácteres"
                     ),
-                    null
+                    maxLength(10, "La contraseña no debe exceder 10 carácteres")
                 ),
-                id_user_type: pipe(
-                    number("El ID de tipo de usuario debe ser un número"),
-                    minValue(
-                        1,
-                        "El ID de tipo de usuario debe ser un número positivo"
-                    )
-                ),
-            }),
-            "El tipo de usuario es obligatorio"
-        ),
-    ]),
-    transform((input) => {
-        if (input.id_user_type !== 1) input.password = null
-        return input
-    })
-)
+                null
+            ),
+            id_user_type: pipe(
+                number("El ID de tipo de usuario debe ser un número"),
+                minValue(
+                    1,
+                    "El ID de tipo de usuario debe ser un número positivo"
+                )
+            ),
+        }),
+        "El tipo de usuario es obligatorio"
+    ),
+])
