ddfc2c9f06a5e425b54f31c35068242fd4cbe59d
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-30 21:53:52 -0600

    Fix: Expose attributes correctly and parse to json string

--- node/src/dtos/filter.dto.ts
+++ node/src/dtos/filter.dto.ts
@@ -1,10 +1,11 @@
-import { Transform, Type } from "class-transformer"
-import { IsArray, IsNumber, IsObject, IsOptional, Min } from "class-validator"
+import { Expose, Transform, Type } from "class-transformer"
+import { IsArray, IsNumber, IsOptional, IsString, Min } from "class-validator"
 
-const transformFilterFn = ({ value }: DTO.TransformFilters) => {
+const transformFilterFn = ({ value = "{}" }: DTO.TransformFilters) => {
     const regex = /[a-z0-9]+(_(gt|gte|lt|lte|eq|like)$)?/i
+    const obj = JSON.parse(value)
     return Object.fromEntries(
-        Object.entries(value)
+        Object.entries(obj)
             .filter(([key]) => regex.test(key))
             .map(([key, val]) => [
                 key,
@@ -13,30 +14,34 @@ const transformFilterFn = ({ value }: DTO.TransformFilters) => {
     )
 }
 
-const transformOrderFn = ({ value }: DTO.TransformOrders) => {
+const transformOrderFn = ({ value = "[]" }: DTO.TransformOrders) => {
     const regex = /[a-z0-9]+(_(asc|desc)$)?/i
-    return value.filter((item) => regex.test(item))
+    const arr: string[] = JSON.parse(value)
+    return arr.filter((item) => regex.test(item))
 }
 
 export class FilterDto {
     @IsOptional()
-    @IsObject()
-    @Type(() => Object)
+    @IsString()
+    @Expose()
     @Transform(transformFilterFn)
-    filters?: Record<string, string | number>
+    declare filters?: Record<string, string | number>
 
+    @Expose()
     @IsOptional()
     @IsArray()
     @Type(() => Array<string>)
     @Transform(transformOrderFn)
     orders?: string[]
 
+    @Expose()
     @IsOptional()
     @Type(() => Number)
     @IsNumber()
     @Min(1)
     limit?: number = 10
 
+    @Expose()
     @IsOptional()
     @Type(() => Number)
     @IsNumber()
