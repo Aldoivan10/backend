8d4e322d6e4039ceb7bc18b3b0f5ee7b30fa3863
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-31 14:37:41 -0600

    Add: Update shortcuts function

--- node/src/controllers/user.ctrl.ts
+++ node/src/controllers/user.ctrl.ts
@@ -1,6 +1,9 @@
+import { SqliteError } from "better-sqlite3"
+import { NextFunction, Request, Response } from "express"
 import { inject, injectable } from "inversify"
 import { Types } from "../containers/types"
 import { UserDTO } from "../dtos/user.dto"
+import { DBError } from "../models/error"
 import { UserService } from "../services/user.svc"
 import { UserSchema } from "../validations/user.val"
 import { Controller } from "./controller"
@@ -21,5 +24,21 @@ export class UserController extends Controller<
         @inject(Types.ProductService) protected readonly svc: UserService
     ) {
         super(["nombre", "role"])
+    }
+
+    public setShortcuts(req: Request, res: Response, next: NextFunction) {
+        try {
+            const id = req.user!.id
+            const { shortcuts } = req.body
+            const updated = this.svc.shortcuts(id, shortcuts)
+            res.json({
+                msg: updated
+                    ? "Aatajos actualizados"
+                    : "No hubo modificaicones",
+            })
+        } catch (err) {
+            if (err instanceof SqliteError) next(DBError.query(err))
+            else next(err)
+        }
     }
 }
