ed8d50e9ca693e1900f9f31b2da19e3b1e2627fb
Aldoivan <aldoivan_palma@hotmail.com>
2024-12-31 12:50:50 -0600

    Fix: Validations structure

--- node/src/validations/catalog.val.ts
+++ node/src/validations/catalog.val.ts
@@ -1,9 +1,15 @@
+import { BaseSchema, InferInput, InferIssue } from "valibot"
 import { NameSchema } from "./general.val"
 
-export default {
+export const CatalogSchemas: Record<
+    string,
+    BaseSchema<any, any, InferIssue<any>>
+> = {
     code: NameSchema(128),
     entity_type: NameSchema(128),
     unit: NameSchema(32),
     department: NameSchema(128),
     user_type: NameSchema(32),
 }
+
+export type CatalogType = InferInput<(typeof CatalogSchemas)["code"]>
--- node/src/validations/product.val.ts
+++ node/src/validations/product.val.ts
@@ -1,113 +1,147 @@
-import { Schema } from "express-validator"
-import { nameAttr } from "../util/val.uti"
+import {
+    array,
+    boolean,
+    check,
+    InferOutput,
+    intersect,
+    minLength,
+    minValue,
+    nonEmpty,
+    nullish,
+    number,
+    object,
+    pipe,
+    required,
+    string,
+    transform,
+    trim,
+} from "valibot"
+import { formatDecimals } from "../utils/number.util"
+import { NameSchema } from "./general.val"
 
-export const productVal: Schema = {
-    name: nameAttr(256),
-    id_department: {
-        isInt: {
-            errorMessage: "El ID del departamento debe ser numérico",
-        },
-    },
-    id_supplier: {
-        isInt: {
-            errorMessage: "El ID del proveedor debe ser numérico",
-        },
-    },
-    amount: {
-        isFloat: {
-            options: { gt: 0 },
-            errorMessage: "La cantidad debe ser númerica mayor a 0",
-        },
-    },
-    buy: {
-        isFloat: {
-            options: { gt: 0 },
-            errorMessage: "El precio de compra debe ser un número mayor a 0",
-        },
-    },
-    min: {
-        isFloat: {
-            options: { gt: 0 },
-            errorMessage: "La cantidad debe ser númerica mayor a 0",
-        },
-    },
-    refundable: {
-        optional: true,
-        isBoolean: {
-            errorMessage: "Reembolsable debe ser un booleano",
-        },
-    },
-    codes: {
-        isArray: {
-            options: {
-                min: 1,
-            },
-            errorMessage:
-                "Los códigos deben ser un arreglo de al menos 1 elemento",
-        },
-        custom: {
-            options: (arr: ProductCode[]) => {
-                const ids = new Set(arr.map((item) => item.id))
-                const codes = new Set(arr.map((item) => item.code))
-                if (ids.size !== arr.length)
-                    throw new Error(
-                        "No puede haber más de un código por tipo de código"
-                    )
-                if (codes.size !== arr.length)
-                    throw new Error("Los códigos deben ser únicos")
-                return true
-            },
-        },
-    },
-    "codes.*.id": {
-        isInt: {
-            options: { gt: 0 },
-            errorMessage: "El ID del código debe ser un número positivo",
-        },
-    },
-    "codes.*.code": {
-        isString: {
-            errorMessage: "El código debe ser una cadena",
-        },
-        trim: true,
-        notEmpty: {
-            errorMessage: "El código es obligatorio",
-        },
-    },
-    units: {
-        isArray: {
-            options: {
-                min: 1,
-            },
-            errorMessage:
-                "Las ventas deben ser un arreglo de al menos 1 elemento",
-        },
-        custom: {
-            options: (arr: ProductUnit[]) => {
-                const ids = new Set(arr.map((item) => item.id))
-                if (ids.size !== arr.length)
-                    throw new Error("Las unidades deben ser únicas")
-                return true
-            },
-        },
-    },
-    "units.0.profit": {
-        isFloat: {
-            options: { gt: 0 },
-            errorMessage:
-                "La ganancia debe ser un número mayor a 0 (Solo para el primer elemento).",
-        },
-    },
-    "units.*.id": {
-        isInt: {
-            options: { gt: 0 },
-            errorMessage: "El ID de la unidad debe ser un número positivo",
-        },
-    },
-    "units.*.sale": {
-        isFloat: {
-            options: { gt: 0 },
-            errorMessage: "El precio de venta debe ser un número mayor a 0",
-        },
-    },
-}
+export const ProductSchema = intersect([
+    NameSchema(256),
+    required(
+        object({
+            id_department: pipe(
+                number(`El ID del departamento debe ser numérico`),
+                minValue(1, `El ID debe ser mayor a 0`)
+            ),
+        }),
+        `El departamento es obligatorio`
+    ),
+    required(
+        object({
+            id_supplier: pipe(
+                number(`El ID del proveedor debe ser numérico`),
+                minValue(1, `El ID debe ser mayor a 0`)
+            ),
+        }),
+        `El proveedor es obligatorio`
+    ),
+    required(
+        object({
+            amount: nullish(
+                pipe(
+                    number("La cantidad de producto debe ser un número"),
+                    minValue(0, "La cantidad mínima de producto es 0"),
+                    transform(formatDecimals)
+                ),
+                0
+            ),
+            min: nullish(
+                pipe(
+                    number("La cantidad mínima debe ser un número"),
+                    minValue(
+                        0,
+                        "La cantidad mínima debe ser igual o mayor que 0"
+                    ),
+                    transform(formatDecimals)
+                ),
+                0
+            ),
+            refundable: nullish(
+                boolean("Reembolsable debe ser un booleano"),
+                false
+            ),
+            buy: pipe(
+                number("El precio de compra debe ser un número"),
+                minValue(0, "El precio de compra mínimo es 0"),
+                transform(formatDecimals)
+            ),
+        }),
+        ["buy"],
+        "El precio de comprar es requerido"
+    ),
+    required(
+        object({
+            codes: pipe(
+                array(
+                    object({
+                        id: pipe(
+                            number("El ID del código debe ser un número"),
+                            minValue(1, "El ID del código debe ser mayor a 0")
+                        ),
+                        code: pipe(
+                            string("El código debe ser una cadena"),
+                            trim(),
+                            nonEmpty("El código no puede estar vacío")
+                        ),
+                    })
+                ),
+                minLength(1, "Debe haber al menos un código"),
+                check((arr) => {
+                    const ids = arr.map((item) => item.id)
+                    const set = new Set(ids)
+                    return ids.length === set.size
+                }, "No se pueden repetir los códigos")
+            ),
+        }),
+        "Los códigos son requeridos"
+    ),
+    required(
+        object({
+            units: pipe(
+                array(
+                    object({
+                        id: pipe(
+                            number("El ID de la unidad debe ser un número"),
+                            minValue(1, "El ID de la unidad debe ser mayor a 0")
+                        ),
+                        sale: pipe(
+                            number("El precio de venta debe ser un número"),
+                            minValue(0, "El precio de venta mínimo es 0"),
+                            transform(formatDecimals)
+                        ),
+                        profit: nullish(
+                            pipe(
+                                number(
+                                    "La ganacia de la unidad debe ser un número (para la primer unidad)"
+                                ),
+                                minValue(
+                                    0,
+                                    "La ganancia de la unidad mínima es 0 (para la primer unidad)"
+                                ),
+                                transform(formatDecimals)
+                            ),
+                            null
+                        ),
+                    })
+                ),
+                minLength(1, "Debe haber al menos una unidad"),
+                check((arr) => {
+                    const ids = arr.map((item) => item.id)
+                    const set = new Set(ids)
+                    return ids.length === set.size
+                }, "No se pueden repetir las unidades"),
+                check(
+                    (arr) => arr[0]?.profit !== null,
+                    "La ganancia es requerida para la primer unidad"
+                )
+            ),
+        }),
+        "Las unidades son requeridas"
+    ),
+])
+
+export type ProductBody = InferOutput<typeof ProductSchema>
